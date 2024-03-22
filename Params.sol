20f892458189f943e6d8c60edc6db036f095c0bf
GiladLeef <96906027+GiladLeef@users.noreply.github.com>
2024-03-22 07:19:09 +0200

    Initial commit

---
+++ Params.sol
@@ -0,0 +1,60 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.8.17;
+
+contract Params {
+    bool public initialized;
+
+    // System contracts
+    address
+        public constant ValidatorContractAddr = 0x000000000000000000000000000000000000f000;
+    address
+        public constant PunishContractAddr = 0x000000000000000000000000000000000000F001;
+    address
+        public constant ProposalAddr = 0x000000000000000000000000000000000000F002;
+
+    // System params
+    uint16 public constant MaxValidators = 2680;
+    // Validator have to wait StakingLockPeriod blocks to withdraw staking
+    uint64 public constant StakingLockPeriod = 2680;
+    uint256 public constant MinimalStakingCoin = 32 ether;
+    // minimum initial staking to become a validator
+    uint256 public minimumValidatorStaking = 16777216 ether;
+
+    modifier onlyMiner() {
+        require(msg.sender == block.coinbase, "Miner only");
+        _;
+    }
+
+    modifier onlyNotInitialized() {
+        require(!initialized, "Already initialized");
+        _;
+    }
+
+    modifier onlyInitialized() {
+        require(initialized, "Not init yet");
+        _;
+    }
+
+    modifier onlyPunishContract() {
+        require(msg.sender == PunishContractAddr, "Punish contract only");
+        _;
+    }
+
+    modifier onlyBlockEpoch(uint256 epoch) {
+        require(block.number % epoch == 0, "Block epoch only");
+        _;
+    }
+
+    modifier onlyValidatorsContract() {
+        require(
+            msg.sender == ValidatorContractAddr,
+            "Validators contract only"
+        );
+        _;
+    }
+
+    modifier onlyProposalContract() {
+        require(msg.sender == ProposalAddr, "Proposal contract only");
+        _;
+    }
+}
